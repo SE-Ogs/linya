4afdd7c960f59b0cadc983fc6b8f56be5834c160
HoogaBoga <matty.lim718@gmail.com>
2025-07-03 17:22:15 +0800

    PLEASE PULLLLL

---
+++ resources/views/layout/user.blade.php
@@ -0,0 +1,20 @@
+
+
+<!DOCTYPE html>
+<html lang=en>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Linya</title>
+
+        @vite('resources/css/app.css')
+        @vite('resources/js/app.js')
+    </head>
+
+    <body>
+        <aside>
+            @include('partials.sidebar')
+        </aside>
+    </body>
+</html>
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,7 @@
+<div class="bg-[#23222E] w-64 h-screen text-white p-4">
+    <h2 class="text-xl mb-4">Sidebar</h2>
+    <ul>
+        <li><a href="/dashboard">Dashboard</a></li>
+        <li><a href="/profile">Profile</a></li>
+    </ul>
+</div>
