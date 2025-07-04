a2a2fd784a1d5ede3d0ff2f0bd6ba0c7b843dacf
HoogaBoga <matty.lim718@gmail.com>
2025-07-04 21:55:32 +0800

    Somewhat Finished Footer

--- resources/views/layout/user.blade.php
+++ resources/views/layout/user.blade.php
@@ -11,27 +11,47 @@
     @vite('resources/js/app.js')
 </head>
 
-<body>
+<body class="min-h-screen flex flex-col">
 
+    <div class="flex justify-between">
+        <button id="toggleSideBar"
+            class="ml-4 mt-3 p-2 h-12 text-[#9A9A9A] hover:bg-gray-100 rounded-[10px] transition duration-300 cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="size-8">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+
+        </button>
+        <div class="m-4 space-x-1">
+            <button type="button" id="signup"
+                class="px-5 py-2 hover:bg-[#4338CA] hover:text-white border-[#4338CA] transition duration-300 border rounded-[6px] text-[14px] text-[#4338CA] cursor-pointer">Sign
+                Up</button>
+            <button type="button" id="login"
+                class="px-5 py-2 hover:bg-[#FF8334] hover:text-white border-[#4338CA] transition duration-300 rounded-[6px] text-[14px] text-[#4B5563] cursor-pointer">Log
+                In</button>
+        </div>
+    </div>
 
     <aside id="sidebar" class="fixed left-0 top-0 z-50 transition-transform duration-300 transform -translate-x-full">
-        @include('partials.dashboard_header')
 
         @include('partials.sidebar')
     </aside>
 
+    <div id="header">
+        @include('partials.dashboard_header')
+    </div>
+
+    <main class="flex-grow">
+
+    </main>
 
 
-    <button id="toggleSideBar"
-        class="m-4 p-2 text-[#9A9A9A] hover:bg-gray-100 rounded-[10px] transition duration-300 cursor-pointer">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-        </svg>
-
-    </button>
-
+    <footer>
+        @include('partials.footer')
+    </footer>
 
 </body>
+
+
 
 </html>
--- resources/views/partials/dashboard_header.blade.php
+++ resources/views/partials/dashboard_header.blade.php
@@ -1,26 +1,8 @@
 <div id="header" class="bg-white">
-    <div id="header_buttons" class="flex flex-row items-center justify-between w-full px-4 mt-4">
-        <!--burgir button-->
-        <div id="hamburger_button" class="w-32 h-20">
-            <button type="button"><img src="/pics/burger_button.svg" alt="burger_button"></button>
-        </div>
-        <!-- spacer -->
-        <div class="flex-grow"></div>
-
-        <!-- login ug signup buttons -->
-        <div class="flex items-center">
-            <div id="signup_button" class="w-32 h-20 mr-[5px]">
-                <button type="button"><img src="/pics/signup.svg" alt="signup_button"></button>
-            </div>
-            <div id="login_button" class="w-32 h-20">
-                <button type="button"><img src="/pics/login.svg" alt="login_button"></button>
-            </div>
-        </div>
-    </div>
-
-    <div id="linya-logo" class="flex flex-col items-center mt-4">
-        <img src="/pics/Linya Student Publication.svg" alt="Linya Student Publication" class="h-auto w-auto">
+    <div id="linya-logo" class="flex flex-col items-center">
+        <hr class="w-full max-w-[450px] md:max-w-[950px] lg:max-w-[1870px] mb-10 border-gray-200 border" />
+        <img src="/images/linya_student_publication.svg" alt="Linya Student Publication" class="h-auto w-auto mb-2">
         <span class="text-[#4B5563]">Student Publication</span>
-        <img src="/pics/line.svg" alt="line pic" class="w-full h-auto mt-3">
+        <hr class="w-full max-w-[450px] md:max-w-[950px] lg:max-w-[1870px] mt-10 border-gray-200 border" />
     </div>
 </div>
