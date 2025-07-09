806d10c4e8f995a06614127081dac22b9ec41851
HoogaBoga <matty.lim718@gmail.com>
2025-07-09 13:51:01 +0800

    Chore: Changed Fonts

--- resources/views/partials/dashboard_header.blade.php
+++ resources/views/partials/dashboard_header.blade.php
@@ -2,7 +2,9 @@
     <div id="linya-logo" class="flex flex-col items-center">
         <hr class="w-full max-w-[450px] md:max-w-[950px] lg:max-w-[1870px] mb-10 border-gray-200 border" />
         <img src="/images/linya_student_publication.svg" alt="Linya Student Publication" class="h-auto w-auto mb-2">
-        <span class="text-[#4B5563]">Student Publication</span>
+        <span class="text-[#4B5563]">
+            <p>Student Publication</p>
+        </span>
         <hr class="w-full max-w-[450px] md:max-w-[950px] lg:max-w-[1870px] mt-10 border-gray-200 border" />
     </div>
 </div>
