c1e78a8c1bb442020e6640eaa959a530f3fdb028
HoogaBoga <matty.lim718@gmail.com>
2025-07-16 07:40:04 +0800

    Chore: Made search pop-up more animated

--- resources/views/partials/search-popup.blade.php
+++ resources/views/partials/search-popup.blade.php
@@ -1,5 +1,5 @@
 <div id="search-popup"
-     class="absolute mx-auto ml-1 mt-3 hidden h-[250px] w-full max-w-[310px] flex-col overflow-auto rounded-[15px] bg-[#F8F6F2] p-4 text-black">
+     class="pointer-events-none absolute mx-auto ml-1 mt-3 flex h-[250px] w-full max-w-[310px] scale-95 flex-col overflow-auto rounded-[15px] bg-[#F8F6F2] p-4 text-black opacity-0 transition duration-300 ease-in-out">
     <!--Hardcoded for now-->
     <h4 class>Recent</h4>
 
@@ -7,7 +7,8 @@
     </hr>
 
     <ul class="text-black/30">
-        <li class="flex items-center space-x-2 pt-2"><svg xmlns="http://www.w3.org/2000/svg"
+        <li class="flex cursor-pointer items-center space-x-2 rounded-2xl p-2 transition duration-300 hover:bg-blue-100">
+            <svg xmlns="http://www.w3.org/2000/svg"
                  fill="none"
                  viewBox="0 0 24 24"
                  stroke-width="1.5"
@@ -18,7 +19,9 @@
                       d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg><span>Software Engineering</span></li>
 
-        <li class="flex items-center space-x-2 pt-2"><svg xmlns="http://www.w3.org/2000/svg"
+        <li
+            class="flex cursor-pointer items-center space-x-2 rounded-2xl p-2 transition duration-300 hover:bg-blue-100">
+            <svg xmlns="http://www.w3.org/2000/svg"
                  fill="none"
                  viewBox="0 0 24 24"
                  stroke-width="1.5"
@@ -30,7 +33,9 @@
             </svg>
             <span>Animation</span>
         </li>
-        <li class="flex items-center space-x-2 pt-2"><svg xmlns="http://www.w3.org/2000/svg"
+        <li
+            class="flex cursor-pointer items-center space-x-2 rounded-2xl p-2 transition duration-300 hover:bg-blue-100">
+            <svg xmlns="http://www.w3.org/2000/svg"
                  fill="none"
                  viewBox="0 0 24 24"
                  stroke-width="1.5"
