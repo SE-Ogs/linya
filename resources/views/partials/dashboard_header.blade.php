17b3a708ada8d3e171c14098a6068f55ca139bb7
HoogaBoga <matty.lim718@gmail.com>
2025-07-17 10:21:22 +0800

    Chore: Logout button works now and user name is dynamic

--- resources/views/partials/dashboard_header.blade.php
+++ resources/views/partials/dashboard_header.blade.php
@@ -1,10 +1,14 @@
-<div id="header" class="bg-white">
-    <div id="linya-logo" class="flex flex-col items-center">
-        <hr class="w-full max-w-[450px] md:max-w-[950px] lg:max-w-[1870px] mb-10 border-gray-200 border" />
-        <img src="/images/linya_student_publication.svg" alt="Linya Student Publication" class="h-auto w-auto mb-2">
+<div id="header"
+     class="bg-white">
+    <div id="linya-logo"
+         class="flex flex-col items-center">
+        <hr class="mb-10 mt-3 w-full max-w-[450px] border border-gray-200 md:max-w-[950px] lg:max-w-[1870px]" />
+        <img src="/images/linya_student_publication.svg"
+             alt="Linya Student Publication"
+             class="mb-2 h-auto w-auto">
         <span class="text-[#4B5563]">
             <p>Student Publication</p>
         </span>
-        <hr class="w-full max-w-[450px] md:max-w-[950px] lg:max-w-[1870px] mt-10 border-gray-200 border" />
+        <hr class="mt-10 w-full max-w-[450px] border border-gray-200 md:max-w-[950px] lg:max-w-[1870px]" />
     </div>
 </div>
